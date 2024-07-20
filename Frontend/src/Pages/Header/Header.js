f9cf9b7b82828e633a5d0dabc509b58a0230a317
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-07-21 02:09:03 +0300

    fix: switching header links appearance

--- Frontend/src/Pages/Header/Header.js
+++ Frontend/src/Pages/Header/Header.js
@@ -1,37 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
-
+  const [showAddProductLinks, setshowAddProductLinks] = useState(false);
   const navigator = useNavigate();
 
-  if (window.location.pathname === "add-product") {
-    alert("ok")
-  }
+  useEffect(() => {
+    if (window.location.pathname === "/add-product") {
+      setshowAddProductLinks(true);
+    } else if (window.location.pathname === "/") {
+      setshowAddProductLinks(false);
+    }
+  });
 
   return (
-    <header id="header" class="fixed-top" style={{'background-color': 'grey'}}>
+    <header
+      id="header"
+      class="fixed-top"
+      style={{ "background-color": "grey" }}
+    >
       <div class="container d-flex align-items-center justify-content-between">
         <h1 class="logo">
-         <Link to="/">         
-            Product List 
-         </Link>
-         </h1>
+          {showAddProductLinks ? (
+            <Link to="/">Product Add</Link>
+          ):(
+            <Link to="/">Product List</Link>
+          )}          
+        </h1>
 
         <nav id="navbar" class="navbar">
-          <ul>
-            <li>
-              <Link to="/add-product">         
-                ADD
-             </Link>              
-            </li>           
-            <li>
-              <a class="nav-link scrollto" href="#contact">
-                MASS DELETE
-              </a>
-            </li>
-          </ul>
+          {showAddProductLinks ? (
+            <ul>
+              <li>
+                <Link to="/save">Save</Link>
+              </li>
+              <li>
+                <Link to="/cancel">Cancel</Link>
+              </li>
+            </ul>
+          ) : (
+            <ul>
+              <li>
+                <Link to="/add-product">ADD</Link>
+              </li>
+              <li>
+                <Link to="/delete-multiple">MASS DELETE</Link>
+              </li>
+            </ul>
+          )}
+
           <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
       </div>
