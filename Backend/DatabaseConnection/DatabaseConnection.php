f3a78f5f9422586064bf98e40e408fd3628e0f4b
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-07-21 23:02:31 +0300

    feat: database connection class

---
+++ Backend/DatabaseConnection/DatabaseConnection.php
@@ -0,0 +1,34 @@
+<?php
+
+class DatabaseConnection
+{
+
+  private static $connection;
+
+  private function __construct()
+  {
+    // to prevent direct instantiation.
+  }
+
+  public static function get_database_instance()
+  {
+    if (self::$connection) {
+      return self::$connection;
+    }
+
+    $db_creds = self::get_database_creds();
+    self::$connection = mysqli_connect($db_creds[0], $db_creds[1], $db_creds[2], $db_creds[3]);
+    return self::$connection;
+  }
+
+  private static function get_database_creds() {
+    $env = parse_ini_file('.env');
+    
+    return [
+      $env["DB_HOST"],
+      $env["DB_USERNAME"], 
+      $env["DB_PASSWORD"],
+      $env["DB_NAME"]
+    ];
+  }
+}
