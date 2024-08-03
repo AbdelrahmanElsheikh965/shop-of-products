b104817c4ad2ad72360418100ab110cb421a50b4
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-08-03 07:44:14 +0300

    feat: update API base URL

--- Frontend/src/APIs/Axios.js
+++ Frontend/src/APIs/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'http://api.minimartstore.great-site.net/',
 });
 
 export default axiosInstance;
